c5d18e388a5a9e9243d7f5ee8b05c9ab7cfef91b
Marc Steiner <git@marcsteiner.me>
2017-04-18 19:52:38 +0200

    Add Updater

---
+++ Titan/Protobufs/Json/GitHubResponse.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+
+namespace Titan.Protobufs.Json
+{
+    public class GitHubResponse
+    {
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("path")]
+        public string Path { get; set; }
+
+        [JsonProperty("sha")]
+        public string Sha { get; set; }
+
+        [JsonProperty("size")]
+        public int Size { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("html_url")]
+        public string HtmlUrl { get; set; }
+
+        [JsonProperty("git_url")]
+        public string GitUrl { get; set; }
+
+        [JsonProperty("download_url")]
+        public string DownloadUrl { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("encoding")]
+        public string Encoding { get; set; }
+
+        [JsonProperty("_links")]
+        public WrappedLinks Links { get; set; }
+
+        public class WrappedLinks
+        {
+
+            [JsonProperty("self")]
+            public string Self { get; set; }
+
+            [JsonProperty("git")]
+            public string Git { get; set; }
+
+            [JsonProperty("html")]
+            public string Html { get; set; }
+
+        }
+
+
+    }
+}
---
+++ Titan/Protobufs/Updater.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using EasyHttp.Http;
+using log4net;
+using Titan.Protobufs.Json;
+
+namespace Titan.Protobufs
+{
+    public class Updater
+    {
+
+        public static readonly ILog Log = LogManager.GetLogger(typeof(Updater));
+
+        public static string Base = "https://raw.githubusercontent.com/SteamRE/SteamKit/master/Resources/Protobufs/csgo/";
+
+        public static DirectoryInfo ProtoDir = new DirectoryInfo(Environment.CurrentDirectory + Path.DirectorySeparatorChar + "protobufs");
+        public static FileInfo HistoryFile = new FileInfo(Path.Combine(ProtoDir.ToString(), "history.sha"));
+
+        public static string[] Protos =
+        {
+            "base_gcmessages.proto",
+            "steammessages.proto",
+            "cstrike15_gcmessages.proto",
+            "gcsdk_gcmessages.proto",
+            "engine_gcmessages.proto"
+        };
+
+        public static void Update()
+        {
+            if(!ProtoDir.Exists)
+            {
+                Log.Debug("Protobuf directory doesn't exist. Creating it...");
+                ProtoDir.Create();
+            }
+
+            foreach(var proto in Protos)
+            {
+                var file = new FileInfo(proto);
+                if(file.Exists)
+                {
+                    file.Delete();
+                }
+
+                using(var webClient = new WebClient())
+                {
+                    Log.DebugFormat("Downloading \"{0}\" Protobuf file.", proto);
+                    webClient.DownloadFile(new Uri(Base + proto), Path.Combine(ProtoDir.ToString(), proto));
+                }
+            }
+
+            Log.Debug("Saving hash for update checking.");
+            SaveHash();
+
+            Log.Info("Successfully updated Protobufs.");
+        }
+
+        public static bool RequiresUpdate()
+        {
+            Log.Debug("Sending Request to GitHub.");
+
+            var client = new HttpClient();
+            client.Request.Accept = "application/vnd.github.v3+json";
+            client.Request.UserAgent = "https://github.com/Marc3842h/Titan-Report";
+            var response = client.Get("https://api.github.com/SteamRE/SteamKit/contents/Resources/Protobufs/csgo/engine_gcmessages.proto");
+
+            var result = response.StaticBody<GitHubResponse>();
+
+            Log.Debug("Successfully awaited.");
+
+            if(HistoryFile.Exists)
+            {
+                var content = File.ReadAllLines(HistoryFile.FullName);
+
+                if(content.Length >= 1)
+                {
+                    var sha = content[0];
+
+                    if(sha == result.Sha)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static void SaveHash()
+        {
+            if(HistoryFile.Exists)
+            {
+                HistoryFile.Delete();
+            }
+
+            var client = new HttpClient();
+            client.Request.Accept = "application/vnd.github.v3+json";
+            client.Request.UserAgent = "https://github.com/Marc3842h/Titan-Report";
+            var response = client.Get("https://api.github.com/SteamRE/SteamKit/contents/Resources/Protobufs/csgo/engine_gcmessages.proto");
+
+            var result = response.StaticBody<GitHubResponse>();
+
+            File.WriteAllText(HistoryFile.FullName, result.Sha);
+        }
+
+    }
+}
