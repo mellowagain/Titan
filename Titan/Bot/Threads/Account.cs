d4292c306a2368810eb3d4016c92bbcb502e2b14
Marc Steiner <git@marcsteiner.me>
2017-04-21 16:54:48 +0200

    Using accounts with Steam Guard is now possible

--- Titan/Bot/Threads/Account.cs
+++ Titan/Bot/Threads/Account.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
+using System.Security.Cryptography;
 using System.Threading;
 using log4net;
 using SteamKit2;
@@ -26,6 +28,12 @@ namespace Titan.Bot.Threads
         public readonly DirectoryInfo SentryDirectory;
         public readonly FileInfo SentryFile;
 
+        public string AuthCode { get; set; }
+        public string TwoFactorCode { get; set; }
+
+        [DefaultValue(0)]
+        public int ReconnectTries { get; private set; }
+
         public Json.Accounts.JsonAccount Json { get; set; }
 
         public SteamClient SteamClient { get; private set; }
@@ -40,6 +48,8 @@ namespace Titan.Bot.Threads
         {
             _username = username;
             _password = password;
+
+            ReconnectTries = 0;
 
             Json = json;
 
@@ -65,6 +75,7 @@ namespace Titan.Bot.Threads
             Callbacks.Subscribe<SteamUser.LoggedOnCallback>(OnLoggedOn);
             Callbacks.Subscribe<SteamUser.LoggedOffCallback>(OnLoggedOff);
             Callbacks.Subscribe<SteamGameCoordinator.MessageCallback>(OnGcMessage);
+            Callbacks.Subscribe<SteamUser.UpdateMachineAuthCallback>(OnMachineAuth);
 
             IsRunning = true;
             SteamClient.Connect();
@@ -85,10 +96,20 @@ namespace Titan.Bot.Threads
             {
                 Log.Debug("Successfully connected to Steam. Logging in...");
 
+                byte[] sentryHash = null;
+                if(SentryFile.Exists)
+                {
+                    var fileBytes = File.ReadAllBytes(SentryFile.ToString());
+                    sentryHash = CryptoHelper.SHAHash(fileBytes);
+                }
+
                 SteamUser.LogOn(new SteamUser.LogOnDetails
                 {
                     Username = _username,
-                    Password = _password
+                    Password = _password,
+                    AuthCode = this.AuthCode,
+                    TwoFactorCode = this.TwoFactorCode,
+                    SentryFileHash = sentryHash
                 });
             }
             else
@@ -100,47 +121,100 @@ namespace Titan.Bot.Threads
 
         public void OnDisconnected(SteamClient.DisconnectedCallback callback)
         {
-            Log.Debug("Successfully disconncected from Steam.");
-            IsRunning = false;
+            ReconnectTries++;
+            if(ReconnectTries <= 5 && !IsSuccess)
+            {
+                Log.DebugFormat("Disconnected from Steam. Retrying in 5 seconds... ({0}/5)", ReconnectTries);
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                SteamClient.Connect();
+            }
+            else
+            {
+                Log.Debug("Successfully disconnected from Steam.");
+                IsRunning = false;
+            }
         }
 
         public void OnLoggedOn(SteamUser.LoggedOnCallback callback)
         {
-            if(callback.Result == EResult.OK)
+            switch(callback.Result)
             {
-                // Success
-                Log.Debug("Successfully logged in. Registering that we're playing CS:GO...");
+                case EResult.OK:
+                    Log.Debug("Successfully logged in. Registering that we're playing CS:GO...");
 
-                var playGames = new ClientMsgProtobuf<CMsgClientGamesPlayed>(EMsg.ClientGamesPlayed);
-                playGames.Body.games_played.Add(new CMsgClientGamesPlayed.GamePlayed
-                {
-                    game_id = 730
-                });
-                SteamClient.Send(playGames);
+                    var playGames = new ClientMsgProtobuf<CMsgClientGamesPlayed>(EMsg.ClientGamesPlayed);
+                    playGames.Body.games_played.Add(new CMsgClientGamesPlayed.GamePlayed
+                    {
+                        game_id = 730
+                    });
+                    SteamClient.Send(playGames);
 
-                Thread.Sleep(5000);
+                    Thread.Sleep(5000);
 
-                Log.Debug("Successfully registered playing CS:GO. Sending client hello to CS:GO services.");
+                    Log.Debug("Successfully registered playing CS:GO. Sending client hello to CS:GO services.");
 
-                var clientHello = new ClientGCMsgProtobuf<CMsgClientHello>((uint) EGCBaseClientMsg.k_EMsgGCClientHello);
-                GameCoordinator.Send(clientHello, 730);
-            }
-            else if(callback.Result == EResult.AccountLogonDenied)
-            {
-                Log.Warn("Account has Steam Guard enabled.");
-                // Steam Guard enabled
-                IsRunning = false;
-            }
-            else
-            {
-                Log.ErrorFormat("Unable to logon to account: {0}: {1}", callback.Result, callback.ExtendedResult);
-                IsRunning = false;
+                    var clientHello = new ClientGCMsgProtobuf<CMsgClientHello>((uint) EGCBaseClientMsg.k_EMsgGCClientHello);
+                    GameCoordinator.Send(clientHello, 730);
+                    break;
+                case EResult.AccountLogonDenied:
+                    Log.Info("Please enter the 2FA code from the Steam App:");
+                    AuthCode = Console.ReadLine();
+                    break;
+                case EResult.AccountLoginDeniedNeedTwoFactor:
+                    Log.InfoFormat("Please enter the auth code sent to email at {0}:", callback.EmailDomain);
+                    TwoFactorCode = Console.ReadLine();
+                    break;
+                case EResult.ServiceUnavailable:
+                    Log.Error("Steam is currently offline. Please try again later.");
+                    IsRunning = false;
+                    break;
+                default:
+                    Log.ErrorFormat("Unable to logon to account: {0}: {1}", callback.Result, callback.ExtendedResult);
+                    IsRunning = false;
+                    break;
             }
         }
 
         public void OnLoggedOff(SteamUser.LoggedOffCallback callback)
         {
             Log.DebugFormat("Successfully logged off from Steam: {0}", callback.Result);
+        }
+
+        public void OnMachineAuth(SteamUser.UpdateMachineAuthCallback callback)
+        {
+            Log.Debug("Updating Sentry file...");
+
+            int size;
+            byte[] hash;
+            using(var fwr = File.Open(SentryFile.ToString(), FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                fwr.Seek(callback.Offset, SeekOrigin.Begin);
+                fwr.Write(callback.Data, 0, callback.BytesToWrite);
+                size = (int) fwr.Length;
+
+                fwr.Seek(0, SeekOrigin.Begin);
+                using(var sha = SHA1.Create())
+                {
+                    hash = sha.ComputeHash(fwr);
+                }
+            }
+
+            SteamUser.SendMachineAuthResponse(new SteamUser.MachineAuthDetails
+            {
+                JobID = callback.JobID,
+                FileName = callback.FileName,
+                BytesWritten = callback.BytesToWrite,
+                FileSize = size,
+                Offset = callback.Offset,
+                Result = EResult.OK,
+                LastError = 0,
+                OneTimePassword = callback.OneTimePassword,
+                SentryFileHash = hash
+            });
+
+            Log.Debug("Successfully updated Sentry hash file.");
         }
 
         public void OnGcMessage(SteamGameCoordinator.MessageCallback callback)
