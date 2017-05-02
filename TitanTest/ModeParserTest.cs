ad182bc11aab9ffba488f472c3dbc36ffab43ddd
Marc Steiner <git@marcsteiner.me>
2017-05-02 20:26:05 +0200

    I'll explain when you're older

---
+++ TitanTest/ModeParserTest.cs
@@ -0,0 +1,43 @@
+﻿using Titan.Bot.Mode;
+using Xunit;
+
+namespace TitanTest
+{
+    public class ModeParserTest
+    {
+
+        [Theory]
+        [InlineData("report")]
+        [InlineData("commend")]
+        public void TestParser(string s)
+        {
+            switch(s)
+            {
+                case "report":
+                    Assert.True(ModeParser.Parse(s) == BotMode.Report);
+                    break;
+                case "commend":
+                    Assert.True(ModeParser.Parse(s) == BotMode.Commend);
+                    break;
+            }
+
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void TestParser(int i)
+        {
+            switch(i)
+            {
+                case 0:
+                    Assert.True(ModeParser.Parse(i) == BotMode.Report);
+                    break;
+                case 1:
+                    Assert.True(ModeParser.Parse(i) == BotMode.Commend);
+                    break;
+            }
+        }
+
+    }
+}
---
+++ TitanTest/ThreadTimeoutTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Titan.Util;
+using Xunit;
+
+namespace TitanTest
+{
+    public class ThreadTimeoutTest
+    {
+
+        [Fact]
+        public void TestTimeout()
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    WaitFor<bool>.Run(TimeSpan.FromSeconds(3), () =>
+                    {
+                        for(;;)
+                        {
+                            // Let the thread run for a infinite timespan so it can time out.
+                        }
+                    });
+                }
+                catch (TimeoutException ex)
+                {
+                    Assert.True(true, $"The thread timed successfully out after {ex.Message} seconds.");
+                }
+
+                Assert.True(false, "The thread never timed out.");
+            });
+        }
+
+    }
+}
