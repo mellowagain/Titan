6950ec81bb178cec2cb7059bc375e636226dc0a4
Marc Steiner <git@marcsteiner.me>
2017-05-22 19:22:20 +0200

    Remap outdated class name in test to new class name

--- TitanTest/ModeParserTest.cs
+++ TitanTest/ModeParserTest.cs
@@ -14,29 +14,13 @@ namespace TitanTest
             switch(s)
             {
                 case "report":
-                    Assert.True(ModeParser.Parse(s) == BotMode.Report);
+                    Assert.True(BotModeParser.Parse(s) == BotMode.Report);
                     break;
                 case "commend":
-                    Assert.True(ModeParser.Parse(s) == BotMode.Commend);
+                    Assert.True(BotModeParser.Parse(s) == BotMode.Commend);
                     break;
             }
 
-        }
-
-        [Theory]
-        [InlineData(0)]
-        [InlineData(1)]
-        public void TestParser(int i)
-        {
-            switch(i)
-            {
-                case 0:
-                    Assert.True(ModeParser.Parse(i) == BotMode.Report);
-                    break;
-                case 1:
-                    Assert.True(ModeParser.Parse(i) == BotMode.Commend);
-                    break;
-            }
         }
 
     }
