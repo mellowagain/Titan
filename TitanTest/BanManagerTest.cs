aa0457f691dac8f8414b71825bbd7e92b533d606
Marc Steiner <git@marcsteiner.me>
2017-05-25 20:06:08 +0200

    Run tests only if the environment variable exists
    Signed-off-by: Marc Steiner <git@marcsteiner.me>

--- TitanTest/BanManagerTest.cs
+++ TitanTest/BanManagerTest.cs
@@ -21,52 +21,65 @@ namespace TitanTest
         [Fact]
         public void TestGameBan()
         {
-            var banInfo = _banManager.GetBanInfoFor(SteamUtil.FromSteamID("STEAM_0:0:208017504"));
+            if(_banManager.APIKey != null)
+            {
 
-            if(banInfo != null && banInfo.GameBanCount > 0)
-            {
-                Assert.True(true, "topkektux has game bans on record.");
+                var banInfo = _banManager.GetBanInfoFor(SteamUtil.FromSteamID("STEAM_0:0:208017504"));
+
+                if(banInfo != null && banInfo.GameBanCount > 0)
+                {
+                    Assert.True(true, "topkektux has game bans on record.");
+                }
+                else
+                {
+                    Assert.True(false);
+                }
+                // STEAM_0:0:208017504
+                // https://steamcommunity.com/id/TopKekTux/
+
             }
-            else
-            {
-                Assert.True(false);
-            }
-            // STEAM_0:0:208017504
-            // https://steamcommunity.com/id/TopKekTux/
         }
 
         [Fact]
         public void TestVacBan()
         {
-            var banInfo = _banManager.GetBanInfoFor(SteamUtil.FromSteamID("STEAM_0:0:19877565"));
+            if(_banManager.APIKey != null)
+            {
 
-            if(banInfo != null && banInfo.VacBanned)
-            {
-                Assert.True(true, "KQLY has VAC bans on record.");
+                var banInfo = _banManager.GetBanInfoFor(SteamUtil.FromSteamID("STEAM_0:0:19877565"));
+
+                if(banInfo != null && banInfo.VacBanned)
+                {
+                    Assert.True(true, "KQLY has VAC bans on record.");
+                }
+                else
+                {
+                    Assert.True(false);
+                }
+                // STEAM_0:0:19877565
+                // https://steamcommunity.com/id/kqly/
             }
-            else
-            {
-                Assert.True(false);
-            }
-            // STEAM_0:0:19877565
-            // https://steamcommunity.com/id/kqly/
         }
 
         [Fact]
         public void TestCleanHistory()
         {
-            var banInfo = _banManager.GetBanInfoFor(SteamUtil.FromSteamID("STEAM_0:0:131983088"));
+            if(_banManager.APIKey != null)
+            {
 
-            if(banInfo != null && (!banInfo.VacBanned && banInfo.GameBanCount <= 0))
-            {
-                Assert.True(true, "Marc3842h has no bans on record.");
+                var banInfo = _banManager.GetBanInfoFor(SteamUtil.FromSteamID("STEAM_0:0:131983088"));
+
+                if(banInfo != null && (!banInfo.VacBanned && banInfo.GameBanCount <= 0))
+                {
+                    Assert.True(true, "Marc3842h has no bans on record.");
+                }
+                else
+                {
+                    Assert.True(false);
+                }
+                // STEAM_0:0:131983088
+                // https://steamcommunity.com/id/Marc3842h/
             }
-            else
-            {
-                Assert.True(false);
-            }
-            // STEAM_0:0:131983088
-            // https://steamcommunity.com/id/Marc3842h/
         }
 
     }
