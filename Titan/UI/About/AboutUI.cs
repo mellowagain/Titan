88631451bcb5429dd4ecf8b33a34d7b09c689fbe
Marc Steiner <git@marcsteiner.me>
2017-08-17 13:35:53 +0200

    Make the license text in the About sub UI scrollable

--- Titan/UI/About/AboutUI.cs
+++ Titan/UI/About/AboutUI.cs
@@ -43,7 +43,7 @@ namespace Titan.UI.About
                         ClientSize = new Size(540, 185);
                         break;
                     case 2:
-                        ClientSize = new Size(540, 575);
+                        ClientSize = new Size(540, 375);
                         break;
                 }
             };
@@ -175,58 +175,72 @@ namespace Titan.UI.About
                     Padding = new Padding(10, 10, 10, 10),
                     Rows =
                     {
-                        new TableRow(
-                            TableLayout.AutoSized(new Label
+                        new Scrollable
+                        {
+                            Content = new TableLayout
                             {
-                                Text = "MIT License",
-                                Font = new Font(SystemFont.Bold),
-                                TextAlignment = TextAlignment.Center,
-                                Wrap = WrapMode.Word
-                            }, centered: true)
-                        ),
-                        new TableRow(
-                            TableLayout.AutoSized(new Label
-                            {
-                                Text = "Copyright (c) 2017 Marc3842h",
-                                TextAlignment = TextAlignment.Center,
-                                Wrap = WrapMode.Word
-                            }, centered: true)
-                        ),
-                        new TableRow(
-                            TableLayout.AutoSized(new Label
-                            {
-                                Text = "Permission is hereby granted, free of charge, to any person obtaining a copy of " +
-                                       "this software and associated documentation files (the \"Software\"), to deal " +
-                                       "in the Software without restriction, including without limitation the rights " +
-                                       "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies " +
-                                       "of the Software, and to permit persons to whom the Software is furnished to do so, " +
-                                       "subject to the following conditions:",
-                                TextAlignment = TextAlignment.Center,
-                                Wrap = WrapMode.Word
-                            }, centered: true)
-                        ),
-                        new TableRow(
-                            TableLayout.AutoSized(new Label
-                            {
-                                Text = "The above copyright notice and this permission notice shall be included in all " +
-                                       "copies or substantial portions of the Software.",
-                                TextAlignment = TextAlignment.Center,
-                                Wrap = WrapMode.Word
-                            }, centered: true)
-                        ),
-                        new TableRow(
-                            TableLayout.AutoSized(new Label
-                            {
-                                Text = "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, " +
-                                       "INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR " +
-                                       "PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE " +
-                                       "FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR " +
-                                       "OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER " +
-                                       "DEALINGS IN THE SOFTWARE.",
-                                TextAlignment = TextAlignment.Center,
-                                Wrap = WrapMode.Word
-                            }, centered: true)
-                        )
+                                Spacing = new Size(5, 5),
+                                Padding = new Padding(10, 10, 10, 10),
+                                Rows =
+                                {
+                                    new TableRow(
+                                        TableLayout.AutoSized(new Label
+                                        {
+                                            Text = "MIT License",
+                                            Font = new Font(SystemFont.Bold),
+                                            TextAlignment = TextAlignment.Center,
+                                            Wrap = WrapMode.Word
+                                        }, centered: true)
+                                    ),
+                                    new TableRow(
+                                        TableLayout.AutoSized(new Label
+                                        {
+                                            Text = "Copyright (c) 2017 Marc3842h",
+                                            TextAlignment = TextAlignment.Center,
+                                            Wrap = WrapMode.Word
+                                        }, centered: true)
+                                    ),
+                                    new TableRow(
+                                        TableLayout.AutoSized(new Label
+                                        {
+                                            Text =
+                                                "Permission is hereby granted, free of charge, to any person obtaining a copy of " +
+                                                "this software and associated documentation files (the \"Software\"), to deal " +
+                                                "in the Software without restriction, including without limitation the rights " +
+                                                "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies " +
+                                                "of the Software, and to permit persons to whom the Software is furnished to do so, " +
+                                                "subject to the following conditions:",
+                                            TextAlignment = TextAlignment.Center,
+                                            Wrap = WrapMode.Word
+                                        }, centered: true)
+                                    ),
+                                    new TableRow(
+                                        TableLayout.AutoSized(new Label
+                                        {
+                                            Text =
+                                                "The above copyright notice and this permission notice shall be included in all " +
+                                                "copies or substantial portions of the Software.",
+                                            TextAlignment = TextAlignment.Center,
+                                            Wrap = WrapMode.Word
+                                        }, centered: true)
+                                    ),
+                                    new TableRow(
+                                        TableLayout.AutoSized(new Label
+                                        {
+                                            Text =
+                                                "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, " +
+                                                "INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR " +
+                                                "PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE " +
+                                                "FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR " +
+                                                "OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER " +
+                                                "DEALINGS IN THE SOFTWARE.",
+                                            TextAlignment = TextAlignment.Center,
+                                            Wrap = WrapMode.Word
+                                        }, centered: true)
+                                    )
+                                }
+                            }
+                        }
                     }
                 }
             };
