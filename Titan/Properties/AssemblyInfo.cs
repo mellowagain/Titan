0794d5b0a80e0ade697fb6108c1a415db333f0b6
Marc3842h <git@marcsteiner.me>
2018-04-09 12:35:41 +0200

    Titan v1.6.1
    Codename: Butterfly

--- Titan/Properties/AssemblyInfo.cs
+++ Titan/Properties/AssemblyInfo.cs
@@ -16,8 +16,8 @@ using System.Runtime.InteropServices;
 #pragma warning disable 7035
 
 // Titan uses SemVer (Microsoft doesn't prefer that and displays a warning), thats why we disable the compiler warnings.
-[assembly: AssemblyVersion("1.6.0")]
-[assembly: AssemblyFileVersion("1.6.0")]
-[assembly: AssemblyInformationalVersion("1.6.0-<%GitHash%>")] // Will be edited by Cake Build Script
+[assembly: AssemblyVersion("1.6.1")]
+[assembly: AssemblyFileVersion("1.6.1")]
+[assembly: AssemblyInformationalVersion("1.6.1-<%GitHash%>")] // Will be edited by Cake Build Script
 
 #pragma warning restore 7035
